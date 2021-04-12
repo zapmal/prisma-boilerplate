54b9030f1ae90db63ad6c64929c1409a5df6da05
Zondazx <mzxgd16@gmail.com>
2021-04-12 19:02:52 -0400

    feat: add prisma schema updater script

---
+++ updateSchemas.sh
@@ -0,0 +1,20 @@
+# If there's no schema.prisma file, this copies all
+# content from prisma/schemas and pastes it in there.
+# Ideally, each schema in prisma/schemas should have spaces in both
+# start and end.
+
+prismaDir='example'
+
+if [ -d $prismaDir/schemas ]; then
+  if [ ! -f $prismaDir/schema.prisma ]; then
+    touch $prismaDir/schema.prisma
+    cat $prismaDir/base.prisma >> example/schema.prisma
+
+    for schema in $(ls example/schemas); do
+      cat $prismaDir/schemas/$schema >> $prismaDir/schema.prisma
+    done
+  else
+    rm $prismaDir/schema.prisma
+    echo 'Re-run the script to update the schema.'
+  fi
+fi
